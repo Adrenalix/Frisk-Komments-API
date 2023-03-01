3e797a96e8ab4cdc780a4e2d13c276e8dde22cec
Kenny Gustavsson <xtromenia@gmail.com>
2023-03-01 18:15:17 +0100

    fixed removal

--- Models/FriskContext.cs
+++ Models/FriskContext.cs
@@ -6,7 +6,6 @@ namespace Frisk_API.Models
     public class FriskContext : DbContext
     {
         public DbSet <Comment> Comments { get; set; }
-        public DbSet <Message> Messages { get; set; }
 
         public FriskContext(DbContextOptions options) : base(options)
         {
