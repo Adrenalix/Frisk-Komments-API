bb07b8156ca33274043138f5334f9cf2ad2d784d
Kenny Gustavsson <xtromenia@gmail.com>
2023-03-01 18:15:10 +0100

    Revert "removal of messages"
    This reverts commit 015cfb6308bf11e37f2ba36e316eaece0e4d3dc4.

---
+++ Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Frisk_API.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderUserId { get; set; }
+        public int RecieverUserId { get; set; }
+        public string TextInput { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
